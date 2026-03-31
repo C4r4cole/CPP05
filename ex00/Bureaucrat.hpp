b98be2850ebdd63fd34a16d11b1f9e590aa2c9b7
C4r4cole <c4r4cole.chevalerie@gmail.com>
2026-03-31 14:35:40 +0200

    ex00 ready for work

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 14:33:05 by fmoulin           #+#    #+#             */
+/*   Updated: 2026/03/31 14:35:00 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+# include <cstdlib>
+
+
+
+# endif
